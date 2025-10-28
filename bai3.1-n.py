6bbdd5952827d5e37c7fe360c0d96a18910eb75f
Nguyễn Bá Nhân <nhanba206@gmail.com>
2025-10-28 13:41:10 +0700

    Delete bai3.1-n.py

--- bai3.1-n.py
+++
@@ -1,9 +0,0 @@
-print("sinh vien: nguyen ba nhan")
-print("msv: 245752021610141")
-def sum(a, b):
-    print("sum = " + str(a + b))
-# tinh tong 2 so 4 va 5
-sum(4, 5)
-# tinh tong 2 so 3 va 7
-sum(3, 7)
-    
